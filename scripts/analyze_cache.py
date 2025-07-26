3f0c1de10fb5231ac93c34e63e79ca317171e77e
nev <nev@neverix.io>
2025-07-26 02:58:23 +0000

    Make final plots

--- scripts/analyze_cache.py
+++ scripts/analyze_cache.py
@@ -40,133 +40,123 @@ import json
 from collections import defaultdict
 import numpy as np
 import matplotlib.pyplot as plt
+import shutil
 
+shutil.rmtree("plots", ignore_errors=True)
 os.makedirs("plots", exist_ok=True)
 
-metric_name = "auc"
-metric_name = "acc"
-# metric_name = "f1"
-metric_nice = dict(
-    auc="ROC AUC",
-    acc="Accuracy",
-    f1="F1",
-)[metric_name]
+for configs, metric_name, info_column in [
+    [["h-last", "h-mean"], "acc", "n_classes"],
+    [["h-mean", "h-attn-8"], "acc", "n_classes"],
+    [["h-mean", "h-attn-1"], "acc", "n_classes"],
+    [["h-attn-1", "h-attn-2"], "entropy", "n_classes"],
+    [["h-attn-2", "h-attn-8"], "entropy", "n_classes"],
+    [["h-attn-1", "h-attn-2"], "acc", "n_classes"],
+    [["h-attn-2", "h-attn-8"], "acc", "n_classes"],
+    [["h-mean-adam", "h-mean"], "acc", "n_classes"],
+    [["h-mean-adam", "h-attn-1"], "acc", "n_classes"],
+    [["h-mean-adam", "h-attn-8"], "acc", "n_classes"],
+    [["hay-mean", "hay-last"], "acc", "n_classes"],
+    [["hay-mean", "hay-attn-1"], "acc", "n_classes"],
+    [["hay-last", "hay-last-adam"], "acc", "n_classes"],
+    [["hay-last", "hay-attn-8"], "acc", "n_classes"],
+    [["hay-attn-1", "hay-attn-2"], "entropy", "n_classes"],
+    [["hay-attn-2", "hay-attn-8"], "entropy", "n_classes"],
+]:
+    metric_nice = dict(
+        auc="ROC AUC",
+        acc="Accuracy",
+        f1="F1",
+        entropy="Entropy ratio",
+    )[metric_name]
 
-info_column = "size"
-info_column = "n_classes"
-
-cache_dir = Path("./cache")
-# configs = ["v1-mean", "v1-8heads"]
-# configs = ["v1-mean", "v1-8heads-no-ft"]
-# configs = ["v1-mean", "v1-mean-ensemble"]
-# configs = ["h-attn", "h-sky"]
-# configs = ["h-attn", "h-mean"]
-# configs = ["hay-attn", "hay-attn-8"]
-# configs = ["hay-attn", "hay-mean"]
-# configs = ["hay-attn", "hay-last"]
-# configs = ["hay-attn-8", "hay-last"]
-# configs = ["h-mean-adam", "h-mean"]
-# configs = ["h-mean-adam", "h-attn-8"]
-# configs = ["h-mean-adam", "h-mean-cv"]
-# configs = ["h-attn-8", "h-mean- cv"]
-# configs = ["h-attn", "h-mean"]
-# configs = ["h-attn-0", "h-mean-0"]
-# configs = ["hay-attn-0", "hay-mean-0"]
-# configs = ["hay-attn-0", "hay-last-0"]
-# configs = ["hay-attn-8-0", "hay-last-0"]
-# configs = ["hay-attn-1", "hay-last"]
-configs = ["h-mean", "h-attn-8"]
-# configs = ["h-mean", "h-mean-adam"]
-# configs = ["hay-attn-8", "hay-last"]
-# configs = ["hay-attn-8", "hay-last-adam"]
-# configs = ["hay-last", "hay-last-adam"]
-# configs = ["hay-mean", "hay-last"]
-results = defaultdict(lambda: defaultdict(list))
-all_eval_results = {}
-all_data_configs = {}
-for config in configs:
-    for suffix in ["-0", "-100", "-200"]:
-        for run in (cache_dir / (config + suffix)).glob("*"):
-            try:
+    cache_dir = Path("./cache")
+    results = defaultdict(lambda: defaultdict(list))
+    all_eval_results = {}
+    all_data_configs = {}
+    for config in configs:
+        for suffix in ["-0", "-100", "-200"]:
+            for run in (cache_dir / (config + suffix)).glob("*"):
                 try:
-                    eval_results = json.load(open(run / "eval_results.json"))
-                except json.JSONDecodeError:
-                    continue
-                all_eval_results[run.name] = eval_results
-                all_data_configs[run.name] = json.load(open(run / "data.json"))
-                if metric_name == "acc":
-                    # results[run.name][config] = np.mean(eval_results["accuracies"])
-                    results[run.name][config].append(eval_results["accuracy"])
-                elif metric_name == "f1":
-                    if eval_results.get("f1s", None) is None:
+                    try:
+                        eval_results = json.load(open(run / "eval_results.json"))
+                    except json.JSONDecodeError:
                         continue
-                    # results[run.name][config] = np.mean(eval_results["f1s"])
-                    results[run.name][config].append(eval_results["f1"])
-                elif metric_name == "auc":
-                    if eval_results.get("roc_aucs", None) is None:
-                        continue
-                    # results[run.name][config] = np.mean(eval_results["roc_aucs"])
-                    results[run.name][config].append(eval_results["roc_auc"])
-            except FileNotFoundError:
-                pass
+                    all_eval_results[run.name] = eval_results
+                    all_data_configs[run.name] = json.load(open(run / "data.json"))
+                    if metric_name == "acc":
+                        results[run.name][config].append(eval_results["accuracy"])
+                    elif metric_name == "f1":
+                        results[run.name][config].append(eval_results["f1"])
+                    elif metric_name == "auc":
+                        results[run.name][config].append(eval_results["roc_auc"])
+                    elif metric_name == "entropy":
+                        results[run.name][config].append(eval_results["entropy"] / eval_results["entropy_baseline"])
+                except FileNotFoundError:
+                    pass
 
-for run_name, config_results in results.items():
-    for config, values in config_results.items():
-        results[run_name][config] = np.mean(values)
+    for run_name, config_results in results.items():
+        for config, values in config_results.items():
+            results[run_name][config] = np.mean(values)
 
-xs = []
-ys = []
-colors = []
-# cmap = plt.get_cmap("tab20")
-for i, (setup, config_results) in enumerate(results.items()):
-    print(setup)
-    for config in results[setup]:
-        print(f"    {config}: {config_results[config]:.2f}")
-    try:
-        config_results[configs[0]], config_results[configs[1]]
-    except KeyError:
-        pass
-    else:
-        xs.append(config_results[configs[0]])
-        ys.append(config_results[configs[1]])
+    xs = []
+    ys = []
+    colors = []
+    # cmap = plt.get_cmap("tab20")
+    for i, (setup, config_results) in enumerate(results.items()):
+        # print(setup)
+        # for config in results[setup]:
+        #     print(f"    {config}: {config_results[config]:.2f}")
         try:
-            colors.append(dataset_infos[all_data_configs[setup]["dataset_path"]][info_column])
+            config_results[configs[0]], config_results[configs[1]]
         except KeyError:
             pass
-        # colors.append(cmap(i))
+        else:
+            xs.append(config_results[configs[0]])
+            ys.append(config_results[configs[1]])
+            try:
+                colors.append(dataset_infos[all_data_configs[setup]["dataset_path"]][info_column])
+            except KeyError:
+                pass
+            # colors.append(cmap(i))
 
-names = {
-    "v1": "Attention Probe",
-    "v1-no-ft": "Attention Probe (not ft'd from mean probe)",
-    "v1-dropout": "Attention Probe w/ dropout",
-    "v1d": "Attention Probe (downsampled)",
-    "v1-last": "Last Token Probe",
-    "v1d-last": "Last Token Probe (downsampled)",
-    "v1-lin-last": "Linear Classifier (Last Token)",
-    "v1-lin-mean": "Linear Classifier (Mean)",
-    "v1-mean": "Mean Probe",
-    "v1-tanh": "Tanh Probe",
-    "h1": "Neurons in a Haystack Attention Probe",
-    "h1-last": "Neurons in a Haystack Last Token Probe",
-    "h1-mean": "Neurons in a Haystack Mean Probe",
-    "v1-mean-ensemble": "Attention Probe ensembled w/ mean probe",
-    "v1-8heads": "Attention Probe w/ 8 heads",
-    "v1-8heads-no-ft": "Attention Probe w/ 8 heads (not ft'd from mean probe)",
-}
-plt.plot([0, 1], [0, 1], "k--")
-if not colors:
-    colors = None
-plt.scatter(xs, ys, c=colors, norm="log")
-plt.xlabel(names.get(configs[0], configs[0]) + f" ({metric_nice})")
-plt.ylabel(names.get(configs[1], configs[1]) + f" ({metric_nice})")
-min_xy = min(min(xs), min(ys))
-max_xy = max(max(xs), max(ys))
-plt.xlim(min_xy, max_xy)
-plt.ylim(min_xy, max_xy)
-if colors is not None:
-    plt.colorbar(label={"size": "Dataset size", "n_classes": "Number of classes"}[info_column], )
-plt.savefig(f"plots/{metric_name}.png")
-plt.show()
+    names = {
+        "h-mean": "Mean Probe",
+        "h-mean-adam": "Mean Probe (Adam)",
+        "h-attn-1": "Attention Probe w/ 1 head",
+        "h-attn-2": "Attention Probe w/ 2 heads",
+        "h-attn-8": "Attention Probe w/ 8 heads",
+        "hay-mean": "NiAH Mean Probe",
+        "hay-last": "NiAH Last Token Probe",
+        "hay-attn-1": "NiAH Attention Probe w/ 1 head",
+        "hay-attn-2": "NiAH Attention Probe w/ 2 heads",
+        "hay-attn-8": "NiAH Attention Probe w/ 8 heads",
+        "hay-mean-adam": "NiAH Mean Probe (Adam)",
+        "h-last": "Last Token Probe",
+        "h-last-adam": "Last Token Probe (Adam)",
+    }
+    plt.plot([0, 1], [0, 1], "k--")
+    if not colors:
+        colors = None
+    plt.scatter(xs, ys, c=colors, norm="log")
+    plt.xlabel(names.get(configs[0], configs[0]) + f" ({metric_nice})")
+    plt.ylabel(names.get(configs[1], configs[1]) + f" ({metric_nice})")
+    min_xy = min(min(xs), min(ys))
+    max_xy = max(max(xs), max(ys))
+    if metric_name != "entropy":
+        min_xy = max(0, min_xy - 0.02)
+        max_xy = 1
+    plt.xlim(min_xy, max_xy)
+    plt.ylim(min_xy, max_xy)
+    
+    if metric_name == "entropy":
+        plt.xlim(0, 1)
+        plt.ylim(0, 1)
+
+    if colors is not None:
+        plt.colorbar(label={"size": "Dataset size", "n_classes": "Number of classes"}[info_column], )
+    plt.savefig(f"plots/{metric_name}_{info_column}_{configs[0]}_{configs[1]}.png")
+    plt.show()
 # %%
 from IPython.display import HTML, display
 html_config = "v1"
